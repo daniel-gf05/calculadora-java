d5fab6fb8c2db34cce971180f3f7860a50390735
daniel-gf05 <danielfgayoso@gmail.com>
2024-12-04 21:59:25 +0100

    README actualizado

---
+++ robot.java/src/robot.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.*;
+import java.util.Scanner;
+public class robot {
+    Scanner sc = new Scanner(System.in);
+    public static void mousePress(int buttons){
+        buttons = 0;
+    }
+    public static void main(String[] args) {
+        mousePress(0);
+    }
+}
